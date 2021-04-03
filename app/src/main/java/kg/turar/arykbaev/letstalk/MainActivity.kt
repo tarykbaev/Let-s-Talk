14db709cecf87565243bea249f60619f79ab3dda
Турар Арыкбаев <turar.arykbaev@gmail.com>
2021-04-03 12:26:13 +0600

    added splash

--- app/src/main/java/kg/turar/arykbaev/letstalk/MainActivity.kt
+++ app/src/main/java/kg/turar/arykbaev/letstalk/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_LetsTalk)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
