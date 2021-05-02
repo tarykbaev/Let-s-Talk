4ab3230393c2a2f7aa227b8ddf68d60e4009f198
Турар Арыкбаев <turar.arykbaev@gmail.com>
2021-05-02 20:51:22 +0600

    refactoring

--- app/src/main/java/kg/turar/arykbaev/letstalk/ui/MainActivity.kt
+++ app/src/main/java/kg/turar/arykbaev/letstalk/ui/MainActivity.kt
@@ -17,25 +17,23 @@ import kg.turar.arykbaev.letstalk.ui.base.BaseActivity
 class MainActivity : BaseActivity<ActivityMainBinding, MainVM>(MainVM::class.java) {
 
     private lateinit var navController: NavController
-    private lateinit var bottomNav: BottomNavigationView
 
+    override fun onResume() {
+        super.onResume()
+        vm.changeUserState(UserState.ONLINE)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         (application as App).appComponent.inject(this)
         super.onCreate(savedInstanceState)
+        setupViews()
+    }
 
-        val fragments = listOf(
-            R.id.login_fragment,
-            R.id.stepOneFragment,
-            R.id.stepTwoFragment,
-            R.id.stepThreeFragment,
-            R.id.verifyMailFragment
-        )
-
+    private fun setupViews() {
         navController = Navigation.findNavController(this, R.id.fragment)
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
-            when (fragments.contains(destination.id)) {
+            when (getHideFragments.contains(destination.id)) {
                 true -> ui.bottomNav.gone()
                 else -> ui.bottomNav.visible()
             }
@@ -44,12 +42,17 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainVM>(MainVM::class.jav
         ui.bottomNav.setupWithNavController(navController)
     }
 
-    override fun performBinding() = ActivityMainBinding.inflate(layoutInflater)
+    private val getHideFragments: List<Int>
+        get() = listOf(
+            R.id.login_fragment,
+            R.id.stepOneFragment,
+            R.id.stepTwoFragment,
+            R.id.stepThreeFragment,
+            R.id.verifyMailFragment,
+            R.id.messageFragment
+        )
 
-    override fun onResume() {
-        super.onResume()
-        vm.changeUserState(UserState.ONLINE)
-    }
+    override fun performBinding() = ActivityMainBinding.inflate(layoutInflater)
 
     override fun onStop() {
         super.onStop()
